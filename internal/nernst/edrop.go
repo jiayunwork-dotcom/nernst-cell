10b737ee06cd1b18be6988e226fd125247d68508
“jiayun” <jiayun_work@163.com>
2026-08-22 15:17:12 +0800

    fix: keep Nernst E and Butler-Volmer current

--- internal/nernst/edrop.go
+++ internal/nernst/edrop.go
@@ -1,8 +1,7 @@
 package nernst
 
 func dropE(v float64) float64 {
-	_ = v
-	return 0
+	return v
 }
 
 func applyE(v float64) float64 {
--- internal/polar/idrop.go
+++ internal/polar/idrop.go
@@ -1,8 +1,7 @@
 package polar
 
 func dropI(v float64) float64 {
-	_ = v
-	return 0
+	return v
 }
 
 func applyI(v float64) float64 {
